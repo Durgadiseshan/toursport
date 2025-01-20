bffd1a29674aa44ab129747c707963cc4006958a
Durga <durga.adiseshan@twilightsoftwares.com>
2025-01-20 15:42:47 +0530

    auth

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/', function () {
 
 
 Route::get('/home', [HomeController::class, 'home']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
